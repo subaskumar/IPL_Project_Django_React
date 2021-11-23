b3a2b4d0562dc35058586c3a13dfe51cece76d5f
subas kumar mallick <87541256+subaskumar@users.noreply.github.com>
2021-11-23 09:02:33 +0530

    firstcommint

--- FrontEnd/ipl_app/src/components/Navbar.js
+++ FrontEnd/ipl_app/src/components/Navbar.js
@@ -1,9 +1,35 @@
 //import ReactDOM from "react-dom";
 import { Link, NavLink } from "react-router-dom";
-//import { GiCricketBat } from "react-icons/gi";
 import { MdSportsCricket } from "react-icons/md";
+import { useState} from "react"
+import { FcSearch } from "react-icons/fc";
+import Axios from 'axios'
+import {useNavigate} from 'react-router-dom';
 
-const NavBar = () => (
+const NavBar = () => {
+    const [search,setSearchplayer] = useState("")
+    const [searchData,setSearchData] = useState({})
+    const navigate = useNavigate()
+    const handleSubmit = (e) =>{
+        e.preventDefault();         // meaning that the default action that belongs to the event will not occur.
+        const inputs = new FormData()
+        inputs.append('teamName',search);
+
+        Axios.post("http://127.0.0.1:8000/search/", inputs)
+            .then((res) => {setSearchData(res.data) 
+                navigate('/search')
+                console.log(searchData)
+
+            })
+            .catch(function(error){
+                    if(error.response){
+                        console.log(error.message);
+                        console.log(error.response);
+            }
+        })
+
+    }
+return (
     <nav className='navbar'>
         <div className='navbar__title'>
             
@@ -15,6 +41,13 @@ const NavBar = () => (
         <div className='navbar__item'>
             <NavLink to="/addTeam">Add Team</NavLink>
         </div>
+       <div className=''>
+            <form className='' onSubmit={handleSubmit} style={{display: "flex"}}>
+                <input id='formPName' className='' name='teamName' type='text' placeholder='Search Team player' onChange={(e)=> setSearchplayer(e.target.value)} value={search} />
+                <button type="submit" style={{fontSize: '25px', cursor: 'pointer', paddingTop: '10x'}}><FcSearch /></button>
+            </form>
+            
+        </div
         
     </nav>
 );
